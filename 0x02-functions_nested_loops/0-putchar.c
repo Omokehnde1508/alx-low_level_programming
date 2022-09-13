9da8e73e49237e80c673dc54fe2e29cc3028f889
Omokehnde1508 <iimmortalworld003@gmail.com>
2022-09-13 12:08:03 -0700

    program that print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,15 @@
-#include "main.h"
 /**
- * main -printing _putchar.
- * Return: 0
+ * main- program that print _putchar
+ * _putchar
+ * Return:1 success
  */
+
+#include "main.h"
 int main(void)
 {
-	putchar("_putchar");
 
-	putchar('\n');
+putchar("_putchar");
+putchar('\n');
 
 return (0);
 }

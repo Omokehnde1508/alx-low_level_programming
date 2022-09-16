7f47047e944a723645334239d0b9fc39d0647349
Omokehnde1508 <iimmortalworld003@gmail.com>
2022-09-16 16:33:26 -0700

    a function that checks for uppercase character.

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,17 @@
-#include "main.h"
+#include"main.h"
+#include <stdio.h>
 
 /**
- * _isupper - checks for uppercase
- * @c:character for checks
- * Return: 1 or 0
+ *  _isupper - function to check if character is uppercase
+ *   @c: checks input of function
+ *   Return: returns 1 if c is uppercase otherwise always 0
  */
 
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
-	{
-		return (1);
-	}
+	if (c >= 'A' && c <= 'Z')
 
-	return (0);
+		return (0);
+	else
+		return (0);
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,28 +1,30 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_INCLUDED 
+#define MAIN_H_INCLUDED 
 
-int _putchar(char c);
+#include <stdio.h> 
 
-int _isupper(int c);
+int _putchar(char c); 
 
-int _isdigit(int c);
+int _isupper(int c); 
 
-int mul(int a, int b);
+int _isdigit(int c); 
 
-void print_numbers(void);
+int mul(int a, int b); 
 
-void print_most_numbers(void);
+void print_numbers(void); 
 
-void more_numbers(void);
+void print_most_numbers(void); 
 
-void print_line(int n);
+void more_numbers(void); 
 
-void print_diagonal(int n);
+void print_line(int n); 
 
-void print_square(int size);
+void print_diagonal(int n); 
 
-void print_triangle(int size);
+void print_square(int size); 
 
-void print_number(int n)
+void print_triangle(int size); 
 
-#endif /* MAIN_H */
+void print_number(int n); 
+
+ #endidif

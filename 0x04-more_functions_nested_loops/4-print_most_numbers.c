b80d4b92f8597daa52411e7e6d041d16506f7e04
Omokehnde1508 <iimmortalworld003@gmail.com>
2022-09-17 00:47:51 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,13 +10,14 @@ void print_most_numbers(void)
 int c;
 
 for (c = 48; c < 58; c++)
-
-if ((c == 50) || (c == 52))
-
 {
-continue;
+if (c != 50)
+{
+if (c != 52)
+{
+_putchar(c);
 }
-_putchar (c);
 }
-_putchar ('\n');
+}
+_putchar('\n');
 }

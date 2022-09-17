a58ee255c164e28189608fd71c67542c7f3a25e7
Omokehnde1508 <iimmortalworld003@gmail.com>
2022-09-16 17:24:25 -0700

    Task 4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
 char _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -14,6 +12,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n)
+void print_number(int n);
 
 #endidif /* MAIN_H */

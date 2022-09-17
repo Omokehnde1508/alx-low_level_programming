e6487cc0c3ec54e00b4a8470b56daa4bbd689a12
Omokehnde1508 <iimmortalworld003@gmail.com>
2022-09-17 05:03:35 -0700

    Fizz-Buzz test” is an interview question

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdio.h>
 /**
  * main - to print fizz buzz
  * Description: printing fizzbuzz
@@ -11,19 +11,28 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
-			printf("FizzBuzz");
-		else if (i % 3 == 0)
-			printf("Fizz");
-		else if (i % 5 == 0)
-			printf("Buzz");
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
 		else
-			printf("%d\n", i);
+		{
+			printf(" %d", i);
 
-		if (i < 100)
-			printf(" ");
-		else
-			printf("\n");
+		}
 	}
+	printf("\n");
 	return (0);
 }

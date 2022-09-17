86d288372a2f49fad48c01b6d284aecc090b71df
Omokehnde1508 <iimmortalworld003@gmail.com>
2022-09-17 04:24:10 -0700

    fizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,16 +7,23 @@
  */
 int main(void)
 {
-	for (int i = 1; i <= 100; i++)
+	int i;
+
+	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
-			printf("FizzBuzz\n");
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Fizz\n");
+			printf("Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz\n");
+			printf("Buzz");
 		else
 			printf("%d\n", i);
+
+		if (i < 100)
+			printf(" ");
+		else
+			printf("\n");
 	}
 	return (0);
 }

75d59a63ff56b42fa55c9a37a5d8a906f82341b6
Omokehnde1508 <iimmortalworld003@gmail.com>
2022-09-19 18:44:40 -0700

    fixed 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,15 @@
 #include "main.h"
- 
+#include <stdio.h>
 /**
  * _puts - function to put character
- * @str: parameter for the function put
- * Return: success
+ * @str: parameter for the function _puts
  */
 
 void _puts(char *str)
 {
 	for (; *str !=  '\0' ; str++)
 	{
-		_putchar(*str);
+		putchar(*str);
 	}
-	_putchar('\n');
+	putchar('\n');
 }

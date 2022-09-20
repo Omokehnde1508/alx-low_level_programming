67dbfe19ac42da2e39b0e0876e3d86bfe5a2c0c5
Omokehnde1508 <iimmortalworld003@gmail.com>
2022-09-19 21:47:07 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,12 +9,30 @@
 void puts_half(char *str)
 
 {
+	int l, n;
 
-	int i, len = _strlen(str);
+	for (l = 0; str[l] != '\0'; ++l)
 
-	for (i = ((len - 1) / 2) + 1; i < len; i++)
 
-		putchar(*(str + i));
+	if (l % 2 == 0)
+
+	{
+
+		for (n = l / 2; str[n] != '\0'; ++n)
+
+			putchar(str[n]);
+
+	}
+
+	else
+
+	{
+
+		for (n = ((l - 1) / 2) + 1; str[n] != '\0'; ++n)
+
+			putchar(str[n]);
+
+	}
 
 	putchar('\n');
 

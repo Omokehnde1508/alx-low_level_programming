d0f612220c71fac214a7311c643f644d83ddfc1c
Adebisi Omokehinde <iimmortalworld003@gmail.com>
2022-11-19 02:49:49 +0100

    done

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,27 +7,25 @@
  *
  * Return: 1 on success and -1 on failure
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, a, b = 0;
+	int o, w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd < 0)
-		return (-1);
-
-	if (text_content)
+	if (text_content != NULL)
 	{
-		while (text_content[b])
-			b++;
-		a = write(fd, text_content, b);
-		if (a != b)
-			return (-1);
+		for (len = 0; text_content[len];)
+			len++;
 	}
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
 
-	close(fd);
+	if (o == -1 || w == -1)
+		return (-1);
+	close(o);
 
 	return (1);
 }
